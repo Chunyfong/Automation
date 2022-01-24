4f33d9900498353dfa50af3c11342317f1509021
Damon Fong <50175081+Chunyfong@users.noreply.github.com>
2022-01-24 09:59:14 +0800

    Update _Package_.py

--- _Package_.py
+++ _Package_.py
@@ -30,8 +30,31 @@ cursor = cnxn.cursor()
 #Table 1
 query = "SELECT *FROM [dbo].[Table1]"
 
+#Date
+# Date
+df['DATE'] = pd.to_datetime(df['DATE'], format='%d/%m/%Y %I:%M:%S %p')
+df['DATE'] = pd.to_datetime(df['DATE'], format='%d/%m/%Y %H:%M:%S.%f')
+# Chop the timestamp
+df['DATE']=df.DATE.dt.date
+# Filter by Period
+df =df[(df['DATE']> "2018-12-30") & (df['DATE']< "2020-01-01")]
+# Return Daily Max Data
+df2 = df.groupby(['ID','DATE'],as_index=False).max()
+
+# Reindex
+filter_df = (df2.set_index('DATE').groupby('ID').apply(lambda d: d.reindex(pd.date_range(min(d.index),max(d.index),freq='D')))
+             .drop('ID', axis=1).reset_index('ID').ffill().bfill().reset_index())
+filter_df.rename(columns={"index":"Date"}, inplace=True)
+
+# Merge
+df1 = m.merge(df2, on = ['ID', 'DATE'], how = 'left')
+#Fill the empty data for Right hand size
+master = master1.ffill().bfill()
+# 
+df1['E'] = df1['E'].fillna(0)
+test['E_Csum'] = test.groupby(['ID'])['E'].apply(lambda x: x.cumsum())
+
 down  = pd.read_excel(' ')
 down  = down ['ID'].values.tolist()
 len(down)
-Ms = totcount.Mach_ID.isin(down)
-filtered_down = totcount[Ms]
+df_check = df2[df2.ID.isin(down)]
