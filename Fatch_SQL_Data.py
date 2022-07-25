801f1113491ef273d7a6a55f2bda74a05d152d5b
Kata1000 <50175081+Chunyfong@users.noreply.github.com>
2022-07-25 14:50:44 +0800

    Update Fatch_SQL_Data.py

--- Fatch_SQL_Data.py
+++ Fatch_SQL_Data.py
@@ -12,6 +12,8 @@ pd.set_option('float_format', '{:f}'.format)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', 16)
 pd.set_option('display.width', 7000)
+pd.set_option('display.colheader_justify', 'center')
+
 
 "{:.2f}".format(5)
 
